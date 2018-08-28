fce5821252d16d21e5cd2641f3482022cb6325e0
Garber <anthony.garber@cardinalhealth.com>
2018-08-28 14:50:43 -0400

    added queryparam

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -5,12 +5,18 @@ import json
 
 keypass = "9254c9a4cecf8d83056b9d5fc0561758"
 
-response = requests.get("https://api.themoviedb.org/3/search/movie?api_key="+keypass+"&query=Jack+Reacher")
+queryparam = "&query=Jack+Reacher"
+
+response = requests.get("https://api.themoviedb.org/3/search/movie?api_key="+keypass+queryparam)
 
 jresponse = response.json()
 
-print(type(jresponse))
+print(jresponse)
 
 #presponse = json.loads(jresponse)
 
-print(jresponse["title"])
+#print(jresponse['original_title'])
+
+#mydict  = {'a':'1','b':'2'}
+
+#print(mydict['b'])

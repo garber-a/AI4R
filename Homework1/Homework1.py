2b0334a5d2ee796e736ee51899b92706a1a80fb2
Garber <anthony.garber@cardinalhealth.com>
2018-08-28 14:26:35 -0400

    working with json formatting

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -1,10 +1,16 @@
 
 import requests
-
+import json
 
 
 keypass = "9254c9a4cecf8d83056b9d5fc0561758"
 
 response = requests.get("https://api.themoviedb.org/3/search/movie?api_key="+keypass+"&query=Jack+Reacher")
 
-print(response.json())
+jresponse = response.json()
+
+print(type(jresponse))
+
+#presponse = json.loads(jresponse)
+
+print(jresponse["title"])

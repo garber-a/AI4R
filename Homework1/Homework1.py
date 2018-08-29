9647bcb7b8e35af72d23c3264b077a6a735a25ea
Garber <anthony.garber@cardinalhealth.com>
2018-08-29 14:23:19 -0400

    fixed genre

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -4,7 +4,7 @@ import json
 
 baseurl = "https://api.themoviedb.org/3/discover/movie?api_key="
 keypass = "9254c9a4cecf8d83056b9d5fc0561758"
-queryparam = "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2000-01-01&with_genres=27"
+queryparam = "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2000-01-01&with_genres=35"
 
 response = requests.get(baseurl + keypass + queryparam)
 

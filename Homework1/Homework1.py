f96676b47747b7dc80bb3e113940263ad7db6ef2
Garber <anthony.garber@cardinalhealth.com>
2018-08-29 14:10:51 -0400

    api filters working

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -2,14 +2,17 @@
 import requests
 import json
 
-
+baseurl = "https://api.themoviedb.org/3/discover/movie?api_key="
 keypass = "9254c9a4cecf8d83056b9d5fc0561758"
+queryparam = "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2000-01-01&with_genres=27"
 
-queryparam = "&query=Jack+Reacher"
-
-response = requests.get("https://api.themoviedb.org/3/search/movie?api_key="+keypass+queryparam)
+response = requests.get(baseurl + keypass + queryparam)
 
 jresponse = response.json()
+
+print(len(jresponse))
+
+print(type(jresponse))
 
 print(jresponse)
 

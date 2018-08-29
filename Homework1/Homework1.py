02cfdfc7215c3ffbbe7bea0bc71f4d327b9397cb
Garber <anthony.garber@cardinalhealth.com>
2018-08-29 14:48:57 -0400

    loop through pages - not appending to dict yet

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -4,17 +4,20 @@ import json
 
 baseurl = "https://api.themoviedb.org/3/discover/movie?api_key="
 keypass = "9254c9a4cecf8d83056b9d5fc0561758"
-queryparam = "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2000-01-01&with_genres=35"
 
-response = requests.get(baseurl + keypass + queryparam)
+jresponse = {}
 
-jresponse = response.json()
+for i in range(1,16):
+    pagenum = i
+    queryparam = "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page="+str(pagenum)+"&primary_release_date.gte=2000-01-01&with_genres=35"
 
-print(len(jresponse))
+    response = requests.get(baseurl + keypass + queryparam)
 
-print(type(jresponse))
+    jresponse.update(response.json())
 
-print(jresponse)
+    print(len(jresponse))
+
+
 
 #presponse = json.loads(jresponse)
 

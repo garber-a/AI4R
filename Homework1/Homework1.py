ae39d86d2f8272fdea2f635f6ae5ee184e8badd1
Anthony Garber <garber.75@gmail.com>
2018-08-29 22:59:17 -0400

    figured out how to iterate through json values. next up write to csv

--- Homework1/Homework1.py
+++ Homework1/Homework1.py
@@ -1,28 +1,30 @@
 
 import requests
 import json
+import csv
 
 baseurl = "https://api.themoviedb.org/3/discover/movie?api_key="
 keypass = "9254c9a4cecf8d83056b9d5fc0561758"
 
-jresponse = {}
+newfile = open('movie_ID_name.csv','w')
+
 
 for i in range(1,16):
     pagenum = i
-    queryparam = "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page="+str(pagenum)+"&primary_release_date.gte=2000-01-01&with_genres=35"
+    
+    queryparam = "&sort_by=popularity.desc&include_video=false&page="+str(pagenum)+"&primary_release_date.gte=2000-01-01&with_genres=35"
 
     response = requests.get(baseurl + keypass + queryparam)
+    jresponse = response.json()
 
-    jresponse.update(response.json())
+    for item in jresponse['results']:
+        print(item['id'])
+    #print(jresponse['results'])
+    #newfile = csv.writer(jresponse['results']['id','title'], delimter=',')
 
-    print(len(jresponse))
+#print(type(jresponse))
+#print(jresponse)
+    
 
 
 
-#presponse = json.loads(jresponse)
-
-#print(jresponse['original_title'])
-
-#mydict  = {'a':'1','b':'2'}
-
-#print(mydict['b'])
